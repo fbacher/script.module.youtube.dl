ef02cb17a663391abfbc58cfc99d56e57e4bcbe7
Wark <wark@free.fr>
2021-03-09 11:10:21 +0100

    Hack 20210309 : Fix extract uploaddate in YT-DL script module Kodi

--- lib/youtube_dl/extractor/youtube.py
+++ lib/youtube_dl/extractor/youtube.py
@@ -1711,9 +1711,10 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
             'formats': formats,
             'thumbnails': thumbnails,
             'description': video_description,
+            # Hack 20210309 : Fix extract uploaddate in YT-DL script module Kodi
             'upload_date': unified_strdate(
-                microformat.get('uploadDate')
-                or search_meta('uploadDate')),
+                microformat.get('uploadDate')),
+                # or search_meta('uploadDate')),
             'uploader': video_details['author'],
             'uploader_id': self._search_regex(r'/(?:channel|user)/([^/?&#]+)', owner_profile_url, 'uploader id') if owner_profile_url else None,
             'uploader_url': owner_profile_url,
